75b928cf267d86fdfd042cdb930a4a19f33baeb3
superlsd <Salvo.lombardo@hotmail.it>
2024-09-17 18:14:40 +0200

    Node2vec

--- Node2vec.py
+++ Node2vec.py
@@ -15,11 +15,11 @@ for e in list(filtered_weighted_exp_graph_significant.edges()):
     weight = filtered_weighted_exp_graph_significant[e[0]][e[1]]["weight"]
     g.add_edge(e[0],e[1],weight)
 g.save("filtered_weighted_exp_graph_significant_new.edg")
-g = node2vec.PreComp(p=0.5, q=1, workers=20, verbose=True,extend=True)
+g = node2vec.PreComp(p=1, q=1, workers=20, verbose=True,extend=True)
 g.read_edg("filtered_weighted_exp_graph_significant_new.edg", weighted=True, directed=False)
 g.preprocess_transition_probs()
 
-filtered_weighted_emd = g.embed(dim=64,num_walks=100,walk_length=50)
+filtered_weighted_emd = g.embed(dim=128,num_walks=200,walk_length=50)
 #Let's write the results
 with open('filtered_weighted_emd.pickle', 'wb') as handle:
     pk.dump(filtered_weighted_emd, handle, protocol=pk.HIGHEST_PROTOCOL)
@@ -29,10 +29,10 @@ for e in list(filtered_unweighted_exp_graph_significant.edges()):
     g.add_edge(e[0],e[1])
 g.save("filtered_unweighted_exp_graph_significant_new.edg")
 
-g = node2vec.PreComp(p=0.5, q=1, workers=20, verbose=True,extend=True)
+g = node2vec.PreComp(p=1, q=1, workers=20, verbose=True,extend=True)
 g.read_edg("filtered_unweighted_exp_graph_significant_new.edg", weighted=False, directed=False)
 g.preprocess_transition_probs()
-filtered_unweighted_emd = g.embed(dim=64,num_walks=100,walk_length=50)
+filtered_unweighted_emd = g.embed(dim=128,num_walks=200,walk_length=50)
 #Let's write the results
 with open('filtered_unweighted_emd.pickle', 'wb') as handle:
     pk.dump(filtered_unweighted_emd, handle, protocol=pk.HIGHEST_PROTOCOL)
@@ -48,10 +48,10 @@ g = AdjlstGraph()
 for e in list(unfiltered_unweighted_exp_graph_significant.edges()):
     g.add_edge(e[0],e[1])
 g.save("unfiltered_unweighted_exp_graph_significant_new.edg")
-g = node2vec.PreComp(p=0.5, q=1, workers=20, verbose=False,extend=True)
+g = node2vec.PreComp(p=1, q=1, workers=20, verbose=False,extend=True)
 g.read_edg("unfiltered_unweighted_exp_graph_significant_new.edg", weighted=False, directed=False)
 g.preprocess_transition_probs()
-unfiltered_unweighted_emd = g.embed(dim=64,num_walks=100,walk_length=50)
+unfiltered_unweighted_emd = g.embed(dim=128,num_walks=200,walk_length=50)
 #Let's write the results
 with open('unfiltered_unweighted_emd.pickle', 'wb') as handle:
     pk.dump(unfiltered_unweighted_emd, handle, protocol=pk.HIGHEST_PROTOCOL)
@@ -61,10 +61,10 @@ for e in list(unfiltered_weighted_exp_graph_significant.edges()):
     weight = unfiltered_weighted_exp_graph_significant[e[0]][e[1]]["weight"]
     g.add_edge(e[0],e[1],weight)
 g.save("unfiltered_weighted_exp_graph_significant_new.edg")
-g = node2vec.PreComp(p=0.5, q=1, workers=20, verbose=False,extend=True)
+g = node2vec.PreComp(p=1, q=1, workers=20, verbose=False,extend=True)
 g.read_edg("unfiltered_weighted_exp_graph_significant_new.edg", weighted=True, directed=False)
 g.preprocess_transition_probs()
-unfiltered_weighted_emd = g.embed(dim=64,num_walks=100,walk_length=50)
+unfiltered_weighted_emd = g.embed(dim=128,num_walks=200,walk_length=50)
 #Let's write the results
 with open('unfiltered_weighted_emd.pickle', 'wb') as handle:
     pk.dump(unfiltered_weighted_emd, handle, protocol=pk.HIGHEST_PROTOCOL)
